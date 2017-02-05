a9f21cdd4a5ea76737cc0bc0eba6ca1f95a83663
kantuni <henrikh.kantuni@gmail.com>
2017-02-05 12:44:41 -0500

    Code Refactoring – Done

--- resizable.js
+++ resizable.js
@@ -6,7 +6,6 @@
 'use strict';
 
 let Resizable = (() => {
-  let handle;
   let wrapper = '<div class="resizable"></div>';
   let handles = '<span class="resize-handle resize-handle-n"></span>\
                  <span class="resize-handle resize-handle-ne"></span>\
@@ -16,22 +15,26 @@ let Resizable = (() => {
                  <span class="resize-handle resize-handle-sw"></span>\
                  <span class="resize-handle resize-handle-w"></span>\
                  <span class="resize-handle resize-handle-nw"></span>';
+  let handle;
   let onInitCallback;
 
-  // element min height and min width
+  // wrapper min height and min width
   let wrapperMinHeight = 50;
   let wrapperMinWidth = 50;
 
-  // remember element height and width to detect change
-  let wrapperOldHeight;
-  let wrapperOldWidth;
+  // remember element's height and width to detect changes
+  let wrapperOldHeight, wrapperOldWidth;
 
   /**
-   * Start element resize, add mouse move and mouse up event listeners
-   * @param event: {Object} mouse down event
+   * Start element resize, add "mouse move" and "mouse up" event listeners
+   * @param event: "mouse down" event
+   * @private
    */
   function _mouseDown(event) {
-    // get handle side
+    // disable selection (Safari)
+    event.preventDefault();
+
+    // get handle direction
     handle = event.target.className.slice('resize-handle resize-handle-'.length);
 
     // add event listeners to mouse move and mouse up
@@ -44,85 +47,106 @@ let Resizable = (() => {
 
   /**
    * Calculate wrapper new height and new width on mouse move
-   * @param event: {Object} mouse move event
+   * @param event: "mouse move" event
+   * @private
    */
   function _mouseMove(event) {
+    // BTDT: this is the right order
     let left = wrapper.getBoundingClientRect().left + document.body.scrollLeft;
     let right = window.innerWidth - left - parseInt(wrapper.style.width, 10);
     let top = wrapper.getBoundingClientRect().top + document.body.scrollTop;
     let bottom = window.innerHeight - top - parseInt(wrapper.style.height, 10);
     let wrapperNewHeight, wrapperNewWidth;
 
+    // BTDT: styles are sorted in clockwise order
     switch (handle) {
-      case 'rotate': {
-
-        break;
-      }
       case 'n': {
-        wrapper.style.top = 'auto';
-        wrapper.style.bottom = bottom + 'px';
+        Object.assign(wrapper.style, {
+          left: left + 'px',
+          top: 'auto',
+          right: right + 'px',
+          bottom: bottom + 'px'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = window.innerHeight - bottom - event.pageY;
         break;
       }
       case 'ne': {
-        wrapper.style.left = left + 'px';
-        wrapper.style.right = 'auto';
-        wrapper.style.top = 'auto';
-        wrapper.style.bottom = bottom + 'px';
+        Object.assign(wrapper.style, {
+          left: left + 'px',
+          top: 'auto',
+          right: 'auto',
+          bottom: bottom + 'px'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = window.innerHeight - bottom - event.pageY;
         wrapperNewWidth = event.pageX - left;
         break;
       }
       case 'e': {
-        wrapper.style.left = left + 'px';
-        wrapper.style.top = top + 'px';
-        wrapper.style.bottom = 'auto';
+        Object.assign(wrapper.style, {
+          left: left + 'px',
+          top: top + 'px',
+          right: 'auto',
+          bottom: bottom + 'px'
+        });
         // adjust wrapper sizes
         wrapperNewWidth = event.pageX - left;
         break;
       }
       case 'se': {
-        wrapper.style.left = left + 'px';
-        wrapper.style.top = top + 'px';
-        wrapper.style.bottom = 'auto';
+        Object.assign(wrapper.style, {
+          left: left + 'px',
+          top: top + 'px',
+          right: 'auto',
+          bottom: 'auto'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = event.pageY - top;
         wrapperNewWidth = event.pageX - left;
         break;
       }
       case 's': {
-        wrapper.style.top = top + 'px';
-        wrapper.style.bottom = 'auto';
+        Object.assign(wrapper.style, {
+          left: left + 'px',
+          top: top + 'px',
+          right: right + 'px',
+          bottom: 'auto'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = event.pageY - top;
         break;
       }
       case 'sw': {
-        wrapper.style.left = 'auto';
-        wrapper.style.right = right + 'px';
-        wrapper.style.top = top + 'px';
-        wrapper.style.bottom = 'auto';
+        Object.assign(wrapper.style, {
+          left: 'auto',
+          top: top + 'px',
+          right: right + 'px',
+          bottom: 'auto'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = event.pageY - top;
         wrapperNewWidth = window.innerWidth - right - event.pageX;
         break;
       }
       case 'w': {
-        wrapper.style.left = 'auto';
-        wrapper.style.right = right + 'px';
-        wrapper.style.top = top + 'px';
-        wrapper.style.bottom = 'auto';
+        Object.assign(wrapper.style, {
+          left: 'auto',
+          top: top + 'px',
+          right: right + 'px',
+          bottom: bottom + 'px'
+        });
         // adjust wrapper sizes
         wrapperNewWidth = window.innerWidth - right - event.pageX;
         break;
       }
       case 'nw': {
-        wrapper.style.left = 'auto';
-        wrapper.style.right = right + 'px';
-        wrapper.style.top = 'auto';
-        wrapper.style.bottom = bottom + 'px';
+        Object.assign(wrapper.style, {
+          left: 'auto',
+          top: 'auto',
+          right: right + 'px',
+          bottom: bottom + 'px'
+        });
         // adjust wrapper sizes
         wrapperNewHeight = window.innerHeight - bottom - event.pageY;
         wrapperNewWidth = window.innerWidth - right - event.pageX;
@@ -131,91 +155,101 @@ let Resizable = (() => {
     }
 
     // don't let wrapper height become less than wrapper min height
-    if (wrapperNewHeight && wrapperNewHeight < wrapperMinHeight) {
+    if (wrapperNewHeight !== undefined && wrapperNewHeight < wrapperMinHeight) {
       wrapperNewHeight = wrapperMinHeight;
     }
 
     // don't let wrapper width become less than wrapper min width
-    if (wrapperNewWidth && wrapperNewWidth < wrapperMinWidth) {
+    if (wrapperNewWidth !== undefined && wrapperNewWidth < wrapperMinWidth) {
       wrapperNewWidth = wrapperMinWidth;
     }
-
+    
     // set new wrapper height
-    wrapper.style.height = wrapperNewHeight + 'px';
-    wrapper.style.width = wrapperNewWidth + 'px';
+    Object.assign(wrapper.style, {
+      height: (wrapperNewHeight !== undefined) ? wrapperNewHeight + 'px' : wrapperOldHeight + 'px',
+      width: (wrapperNewWidth !== undefined) ? wrapperNewWidth + 'px' : wrapperOldWidth + 'px'
+    });
   }
 
   /**
-   * Finish element resize, remove mouse move and mouse up event listeners
-   * @param event: {Object} mouse up event
+   * Finish element resize, remove "mouse move" and "mouse up" event listeners
+   * @param event: "mouse up" event
+   * @private
    */
   function _mouseUp(event) {
-    // remove mouse move and mouse up events
+    // remove "mouse move" and "mouse up" events
     document.removeEventListener('mousemove', _mouseMove);
     document.removeEventListener('mouseup', _mouseUp);
 
     let wrapperNewHeight = wrapper.offsetHeight;
     let wrapperNewWidth = wrapper.offsetWidth;
 
-    // if nothing changed
+    // if nothing has changed
     if (wrapperNewHeight === wrapperOldHeight && wrapperNewWidth === wrapperOldWidth) {
       return;
     }
 
-    // redefine wrapper old height and old width
+    // set wrapper old height and old width
     wrapperOldHeight = wrapperNewHeight;
     wrapperOldWidth = wrapperNewWidth;
   }
 
   /**
-   * Wrap an element with handles and make it resizable
-   * @param element: {Object} DOM element
-   * @param callback: {Function} on init callback
-   * @return: {Object} module (allow chaining)
+   * Create DOM elements for the wrapper, element, and handles.
+   * @param element: a DOM element to resize
+   * @private
    */
-  let init = (element, callback) => {
+  function _createDOMElements(element) {
     // add wrapper near element
     element.insertAdjacentHTML('beforebegin', wrapper);
     wrapper = element.previousSibling;
 
-    // insert handles into wrapper
+    // insert handles into the wrapper
     wrapper.insertAdjacentHTML('beforeend', handles);
 
-    // insert element into wrapper
+    // insert element into the wrapper
     wrapper.appendChild(element);
+  }
 
-    // style wrapper
-    wrapper.style.left = element.style.left;
-    wrapper.style.top = element.style.top;
-
-    // set wrapper height
-    if (parseInt(element.offsetHeight, 10) > wrapperMinHeight) {
-      wrapper.style.height = element.offsetHeight + 'px';
-    } else {
-      wrapper.style.height = wrapperMinHeight + 'px'
-    }
-
-    // set wrapper width
-    if (parseInt(element.offsetWidth, 10) > wrapperMinWidth) {
-      wrapper.style.width = element.offsetWidth + 'px';
-    } else {
-      wrapper.style.width = wrapperMinWidth + 'px'
-    }
+  /**
+   * Apply styles to wrapper and element.
+   * @param element: a DOM element to resize
+   * @private
+   */
+  function _applyStyles(element) {
+    let elementHeight = parseInt(element.offsetHeight, 10);
+    let elementWidth = parseInt(element.offsetWidth, 10);
 
     // set wrapper min height and min width
-    wrapper.style.minHeight = wrapperMinHeight + 'px';
-    wrapper.style.minWidth = wrapperMinWidth + 'px';
+    Object.assign(wrapper.style, {
+      height: (elementHeight > wrapperMinHeight) ? elementHeight + 'px' : wrapperMinHeight + 'px',
+      left: element.style.left,
+      // minHeight: wrapperMinHeight + 'px',
+      // minWidth: wrapperMinWidth + 'px',
+      position: 'absolute',
+      top: element.style.top,
+      width: (elementWidth > wrapperMinWidth) ? elementWidth + 'px' : wrapperMinWidth + 'px'
+    });
 
     // style element
-    element.style.position = 'relative';
-    element.style.left = '0px';
-    element.style.top = '0px';
-    element.style.height = '100%';
-    element.style.width = '100%';
+    Object.assign(element.style, {
+      height: '100%',
+      left: '0',
+      position: 'relative',
+      top: '0',
+      width: '100%'
+    });
 
     wrapperOldHeight = wrapper.offsetHeight;
     wrapperOldWidth = wrapper.offsetWidth;
+  }
 
+  /**
+   * Attach events on init.
+   * @param element: a DOM element to resize
+   * @private
+   */
+  function _attachInitEvents(element) {
     // allow resize after click
     document.addEventListener('mousedown', (event) => {
       let allowed = event.target === element;
@@ -232,19 +266,32 @@ let Resizable = (() => {
     });
 
     handles = wrapper.querySelectorAll('.resize-handle');
-    for (let i = 0; i < handles.length; i++) {
-      // disable default drag start event handler
-      // FF bug here
+    for (let i = 0; i < handles.length; ++i) {
+      // disable default "drag start" event handler
       handles[i].addEventListener('dragstart', {});
-      // add custom mouse down event handler
+      // add custom "mouse down" event handler
       handles[i].addEventListener('mousedown', _mouseDown);
     }
+  }
 
-    // assign on init callback
-    onInitCallback = callback;
+  /**
+   * Wrap an element with handles and make it resizable.
+   * @param element: a DOM element to resize
+   * @param callback: "on init" callback
+   * @return: {*} a Resizable element
+   */
+  let init = (element, callback) => {
+    // create necessary DOM elements
+    _createDOMElements(element);
+
+    // style wrapper and element
+    _applyStyles(element);
+
+    // attach events on init
+    _attachInitEvents(element);
 
     // callback call
-    if (onInitCallback) {
+    if (onInitCallback = callback) {
       onInitCallback();
     }
 
