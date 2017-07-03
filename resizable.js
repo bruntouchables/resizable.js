4c2c8db90fdc558bb9808aaed8601f6abc7e763f
kantuni <henrikh.kantuni@gmail.com>
2017-07-03 17:37:44 +0400

    Use Parent - NW

--- resizable.js
+++ resizable.js
@@ -444,9 +444,11 @@ class Resizable {
         break;
       }
       case 'nw': {
-        let alpha = Math.atan2(e.pageX / this._scale - this.wrapperCenter.x, -e.pageY / this._scale + this.wrapperCenter.y);
+        let x = e.pageX / this._scale - this.parentClientRect.left;
+        let y = e.pageY / this._scale - this.parentClientRect.top;
+        let alpha = Math.atan2(x - this.wrapperCenter.x, -y + this.wrapperCenter.y);
         let angle = this.angle * (Math.PI / 180);
-        let d = Math.sqrt(Math.pow(e.pageY / this._scale - this.wrapperCenter.y, 2) + Math.pow(e.pageX / this._scale - this.wrapperCenter.x, 2));
+        let d = Math.sqrt(Math.pow(y - this.wrapperCenter.y, 2) + Math.pow(x - this.wrapperCenter.x, 2));
         let dh = d * Math.cos(alpha - angle);
         let dw = d * Math.sin(alpha - angle);
         let dx = Math.cos(angle) * this.wrapperClientRect.width / 2 - Math.sin(angle) * this.wrapperClientRect.height / 2;
